adac444e2c4dc5cf89737c5cb2a58671765021b4
RonakJNVKCT <95160124+RonakJNVKCT@users.noreply.github.com>
2022-07-27 23:24:14 +0530

    quick sort

--- sort.c
+++ sort.c
@@ -93,6 +93,50 @@ void merge_sort(int array[], int l, int r){
     merge(array, l, m, r);
   }
 }
+int partition (int arr[], int low, int high){
+int pivot = arr[high];
+int i = (low - 1);
+for (int j = low; j <= high- 1; j++)
+{
+
+if (arr[j] <= pivot)
+{
+  
+i++;
+
+int temp = arr[i];
+arr[i] = arr[j];
+arr[j] = temp;
+
+}
+
+}
+
+int temp = arr[i+1];
+  arr[i+1] = arr[high];
+  arr[high] = temp;
+return (i + 1);
+
+}
+
+void quickSort(int arr[], int low, int high)
+
+{
+
+if (low < high)
+
+{
+
+int pi = partition(arr, low, high);
+
+quickSort(arr, low, pi - 1);
+
+quickSort(arr, pi + 1, high);
+
+}
+
+}
+
 int main(){
   printf("This is a programme to sort an array using various methods");
   int n;
@@ -102,6 +146,7 @@ int main(){
   for(int i = 0;i<n;i++){
     scanf("%d", &numbers[i]);
   }
+  bubble_sort(numbers, n);
   return 0;
 }
  
